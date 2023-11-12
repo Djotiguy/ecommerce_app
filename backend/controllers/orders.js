235e0d438d144e9fc7d395ad9b424ddb9fbdd546
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-11-13 00:30:06 +0100

    feat(controller) addPanier addOrder endpoint

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -2,7 +2,7 @@ import { sql } from "../db.js";
 
 export async function getOrders(_, res) {
   try {
-    const orders = await sql("SELECT * FROM commande");
+    const orders = await sql("SELECT * FROM commande_ligne");
     return res.status(200).json({ data: orders });
   } catch (error) {
     return res.status(400).send({ error: "Bad request" });
@@ -12,7 +12,7 @@ export async function getOrders(_, res) {
 export async function getAnOrder(req, res) {
   try {
     const orderId = req.params.id;
-    const order = await sql(`SELECT * FROM commande WHERE id = ${orderId}`);
+    const order = await sql(`SELECT * FROM commande_ligne WHERE id = ${orderId}`);
     if (order.length === 0) {
       return res.status(400).json({ error: "Order not found" });
     }
@@ -24,6 +24,39 @@ export async function getAnOrder(req, res) {
   }
 }
 
-export async function addOrder() {
-    
+export async function addPanier(req, res) {
+  try {
+    const price = req.body.price;
+    const quantite = req.body.quantity;
+
+    const query = `INSERT INTO public.commande_ligne ("prix_unitaire", "quantite") VALUES (${price}, ${quantite})`;
+
+    const basket = await sql(query);
+
+    return res.status(200).send('commande_ligne added');
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Error Server" });
+  }
 }
+
+export async function addOrder(req, res) {
+  try {
+    const price = req.body.price;
+    const status = "validate"; // Vous pouvez définir la valeur du statut ici
+
+    // Utilisez un paramètre lié pour éviter les injections SQL
+    const query = `INSERT INTO commande (status, prix_total) VALUES('${status}', ${price}) RETURNING *`;
+    
+    // Exécutez la requête SQL avec les paramètres liés
+    const order = await sql(query, [status, price]);
+
+    console.log('Order validated');
+    return res.status(200).json(order);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json("Bad request");
+  }
+}
+
+ 
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getProducts, getOneProduct } from "../controllers/products.js";
-import { getAnOrder, getOrders } from "../controllers/orders.js";
+import { addOrder, addPanier, getAnOrder, getOrders } from "../controllers/orders.js";
 
 const router = express.Router();
 
@@ -8,6 +8,9 @@ router.get("/products", getProducts);
 router.get("/products/:id", getOneProduct);
 router.get("/orders", getOrders);
 router.get("/orders/:id", getAnOrder);
+
+router.post("/orders", addPanier);
+router.post("/orders/:id", addOrder);
 
 export default router;
  
